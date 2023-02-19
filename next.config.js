dfe11d4a514c10c68cce0dc0ee871ba9644f3c22
Mike-is-coding <miguel.fierro750@gmail.com>
2023-02-19 12:22:13 -0800

    Deleted config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,5 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
-  images: {
-    unoptimized: true,
-  },
-}
 
 module.exports = nextConfig
